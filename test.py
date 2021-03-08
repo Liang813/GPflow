0325997751716b0c8039e0d9297e4117bcf9c3e5
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-08 21:06:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,24 @@
 import tensorflow as tf
 from gpflow.quadrature import ndiag_mc
+try:
+    N = 10
+    X_dim = 2
+    Z_dim = 2
+    Y_dim = 4
 
-N = 10
-X_dim = 2
-Z_dim = 2
-Y_dim = 4
+    def func(Z, X=None, Y=None):
+        XZ = tf.concat([X, Z], axis=1)  # N x (X_dim + Z_dim)
+        return tf.reduce_sum(XZ - Y, axis=1)  # N x 1
 
-def func(Z, X=None, Y=None):
-    XZ = tf.concat([X, Z], axis=1)  # N x (X_dim + Z_dim)
-    return tf.reduce_sum(XZ - Y, axis=1)  # N x 1
+    Z_mu = tf.zeros((N, Z_dim), dtype=tf.float64)
+    Z_var = tf.ones((N, Z_dim), dtype=tf.float64)
 
-Z_mu = tf.zeros((N, Z_dim), dtype=tf.float64)
-Z_var = tf.ones((N, Z_dim), dtype=tf.float64)
+    X = tf.random_normal((N, X_dim), dtype=tf.float64)
+    Y = tf.random_normal((N, Y_dim), dtype=tf.float64)
 
-X = tf.random_normal((N, X_dim), dtype=tf.float64)
-Y = tf.random_normal((N, Y_dim), dtype=tf.float64)
+    Ez = ndiag_mc(func, 1000, Z_mu, Z_var, logspace=False, X=X, Y=Y)
 
-Ez = ndiag_mc(func, 1000, Z_mu, Z_var, logspace=False, X=X, Y=Y)
-
-print(tf.Session().run(Ez))
+    print(tf.Session().run(Ez))
+except Exception as e:
+    print("ValueError")
+    print(e)
