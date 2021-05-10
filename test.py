018575e689cbe3e1a0021fa1632d71e4cd951760
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-05-10 10:10:04 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import numpy as np
 import GPflow
-
-rng = np.random.RandomState(1)
-X = rng.randn( 10, 1)
-Y = rng.randn( 10, 1 ) 
-Z = rng.randn( 3,1 )
-model = GPflow.svgp.SVGP( X=X, Y=Y, kern=GPflow.kernels.RBF(1) ,likelihood=GPflow.likelihoods.Gaussian() , Z=Z )
-model.compute_log_likelihood()
+import traceback
+try:
+  rng = np.random.RandomState(1)
+  X = rng.randn( 10, 1)
+  Y = rng.randn( 10, 1 ) 
+  Z = rng.randn( 3,1 )
+  model = GPflow.svgp.SVGP( X=X, Y=Y, kern=GPflow.kernels.RBF(1) ,likelihood=GPflow.likelihoods.Gaussian() , Z=Z )
+  model.compute_log_likelihood()
+except Exception as e:
+  traceback.print_exc(file=open('/script/gpflow99-buggy.txt','w+'))
