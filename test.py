67e33e0f1a79c74bda453ce2b9b8dd356c7ffaf8
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-05-27 20:00:33 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import gpflow
+
+x = np.linspace(0.0, 0.5)
+y = np.sin(x) + 1e-2*np.random.standard_normal(x.shape)
+
+# var_val = 1e-2
+var_val = 1e-7
+
+model = gpflow.models.GPR(data = (x,y),
+                          kernel = gpflow.kernels.RBF(),
+                          noise_variance = var_val)
+
+gpflow.utilities.print_summary(model)
