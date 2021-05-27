721f13f6f851e57ca29fa17a5e9005bd977f2f30
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-05-27 20:13:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ x = np.linspace(0.0, 0.5)
 y = np.sin(x) + 1e-2*np.random.standard_normal(x.shape)
 
 # var_val = 1e-2
-var_val = 1e-7
+var_val = 1e-5
 
 model = gpflow.models.GPR(data = (x,y),
                           kernel = gpflow.kernels.RBF(),
