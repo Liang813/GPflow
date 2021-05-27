5e8f8d0f35fd8663d9edebd46d1edc8d0f9c6ed0
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-05-27 20:38:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 import numpy as np
 import gpflow
+import traceback
+try:
+  x = np.linspace(0.0, 0.5)
+  y = np.sin(x) + 1e-2*np.random.standard_normal(x.shape)
 
-x = np.linspace(0.0, 0.5)
-y = np.sin(x) + 1e-2*np.random.standard_normal(x.shape)
+  # var_val = 1e-2
+  var_val = 1e-7
 
-# var_val = 1e-2
-var_val = 1e-7
+  model = gpflow.models.GPR(data = (x,y),
+                            kernel = gpflow.kernels.RBF(),
+                            noise_variance = var_val)
 
-model = gpflow.models.GPR(data = (x,y),
-                          kernel = gpflow.kernels.RBF(),
-                          noise_variance = var_val)
-
-gpflow.utilities.print_summary(model)
+  gpflow.utilities.print_summary(model)
+except Exception as e:
+  traceback.print_exc(file=open('/script/gpflow1407-buggy.txt','w+'))
