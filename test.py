16f2ffe6c97f55241dce2d78ed94f9275da28194
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-05-27 20:37:04 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,11 @@ x = np.linspace(0.0, 0.5)
 y = np.sin(x) + 1e-2*np.random.standard_normal(x.shape)
 
 # var_val = 1e-2
-var_val = 1e-5
+var_val = 1e-7
 
 model = gpflow.models.GPR(data = (x,y),
                           kernel = gpflow.kernels.RBF(),
                           noise_variance = var_val)
 
-gpflow.utilities.print_summary(model)
+# gpflow.utilities.print_summary(model)
+print("Warnning,The variance of the Gaussian likelihood must be strictly greater than 1e-06")
